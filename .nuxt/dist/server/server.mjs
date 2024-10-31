ea04c7ae4b2b22d2503a99e3ef8227607a8d39c4
Emanuil Gerganov <emanuilgerganov@Emanuils-MacBook-Pro.local>
2024-11-01 00:05:03 +0200

    update master

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/emanuilgerganov/dev/guest-house/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ .nuxt/i18n.options.mjs
@@ -0,0 +1,101 @@
+
+// @ts-nocheck
+
+
+export const localeCodes =  [
+  "bg",
+  "en"
+]
+
+export const localeLoaders = {
+  "bg": [],
+  "en": []
+}
+
+export const vueI18nConfigs = [
+  () => import("../i18n.config.ts?hash=bffaebcb&config=1" /* webpackChunkName: "i18n_config_bffaebcb" */)
+]
+
+export const nuxtI18nOptions = {
+  "experimental": {
+    "localeDetector": "",
+    "switchLocalePathLinkSSR": false,
+    "autoImportTranslationFunctions": false
+  },
+  "bundle": {
+    "compositionOnly": true,
+    "runtimeOnly": false,
+    "fullInstall": true,
+    "dropMessageCompiler": false
+  },
+  "compilation": {
+    "jit": true,
+    "strictMessage": true,
+    "escapeHtml": false
+  },
+  "customBlocks": {
+    "defaultSFCLang": "json",
+    "globalSFCScope": false
+  },
+  "vueI18n": "",
+  "locales": [
+    {
+      "code": "bg",
+      "language": "bg-BG"
+    },
+    {
+      "code": "en",
+      "language": "en-US"
+    }
+  ],
+  "defaultLocale": "bg",
+  "defaultDirection": "ltr",
+  "routesNameSeparator": "___",
+  "trailingSlash": false,
+  "defaultLocaleRouteNameSuffix": "default",
+  "strategy": "prefix_except_default",
+  "lazy": false,
+  "langDir": null,
+  "detectBrowserLanguage": {
+    "alwaysRedirect": false,
+    "cookieCrossOrigin": false,
+    "cookieDomain": null,
+    "cookieKey": "i18n_redirected",
+    "cookieSecure": false,
+    "fallbackLocale": "",
+    "redirectOn": "root",
+    "useCookie": true
+  },
+  "differentDomains": false,
+  "baseUrl": "",
+  "dynamicRouteParams": false,
+  "customRoutes": "page",
+  "pages": {},
+  "skipSettingLocaleOnNavigate": false,
+  "types": "composition",
+  "debug": false,
+  "parallelPlugin": false,
+  "multiDomainLocales": false,
+  "i18nModules": []
+}
+
+export const normalizedLocales = [
+  {
+    "code": "bg",
+    "language": "bg-BG",
+    "files": []
+  },
+  {
+    "code": "en",
+    "language": "en-US",
+    "files": []
+  }
+]
+
+export const NUXT_I18N_MODULE_ID = "@nuxtjs/i18n"
+export const parallelPlugin = false
+export const isSSG = false
+
+export const DEFAULT_DYNAMIC_PARAMS_KEY = "nuxtI18n"
+export const DEFAULT_COOKIE_KEY = "i18n_redirected"
+export const SWITCH_LOCALE_PATH_LINK_IDENTIFIER = "nuxt-i18n-slp"
